9bdef636231c7e740102d11996477227856a3c89
SOL KANG <anmenmolsu22@naver.com>
2015-11-19 20:29:51 +0900

    make GamePanel, CutterGame과 관련된 것들 제거

--- cutterGame/src/cutterGame/CutterGame.java
+++ cutterGame/src/cutterGame/CutterGame.java
@@ -3,8 +3,6 @@ package cutterGame;
 import java.awt.Canvas;
 import java.awt.CardLayout;
 import java.awt.Color;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.image.BufferStrategy;
 import java.util.LinkedList;
 
@@ -13,50 +11,39 @@ import javax.swing.JPanel;
 
 public class CutterGame {
 	private final int gameWidth = 500, gameHeight = 350;
-
+	
 	private JFrame mainFrame;
 	private CardLayout cardLayout ;
-	private Canvas canvas;
-	private BufferStrategy bufferStrategy;
-	private JPanel startPanel, gamePanel, endPanel;
-	private LinkedList<Throwee> throweeList;
-	private Thread physicsCalculation;
+	private JPanel startPanel, endPanel;
+	private GamePanel gamePanel;
+	
+
 	
 	public CutterGame(){
-		throweeList = new LinkedList<>();
 		cardLayout = new CardLayout();
 		mainFrame= new JFrame("CutterGame");
 		mainFrame.setLayout(cardLayout);
 		
-		canvas = new Canvas();
-		canvas.setBackground(Color.white);
-		canvas.setSize(gameWidth,gameHeight);
-
 		startPanel = new JPanel();//sung bin
-		gamePanel = new JPanel();
+		gamePanel = new GamePanel(gameWidth, gameHeight);
 		endPanel = new JPanel(); // wait...
 		
 //		startPanel.add
-		gamePanel.add(canvas);
 //		endPanel.add
 		
-
 		mainFrame.add(startPanel,"start");
 		mainFrame.add(gamePanel,"game");
 //		mainFrame.add(endPanel,"end");
 		
-		
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setSize(gameWidth, gameHeight);
 		mainFrame.setLocationRelativeTo(null);
 		mainFrame.pack();
 		mainFrame.setVisible(true);
-		
-		canvas.createBufferStrategy(2);//visible 이전에 하면 안됨
-		bufferStrategy = canvas.getBufferStrategy();
 	}
 	public void start() {
-		cardLayout.show(mainFrame.getContentPane(),"game"); //아직 start 아님
+//		cardLayout.first(mainFrame.getContentPane());
+		cardLayout.show(mainFrame.getContentPane(),"game");
+		gamePanel.start();
 		
 	}
 }
--- cutterGame/src/cutterGame/GamePanel.java
+++ cutterGame/src/cutterGame/GamePanel.java
@@ -1,12 +1,32 @@
 package cutterGame;
 
-import javax.swing.JButton;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.image.BufferStrategy;
+import java.util.LinkedList;
 
-public class GamePanel {
-	JButton button;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+	private int gameWidth, gameHeight;
+	private BufferStrategy bufferStrategy;
+	private LinkedList<Throwee> throweeList;
+	private Thread physicsCalculation;
+	private Canvas canvas;
 	
-	public GamePanel() {
-		button = new JButton("Start");
+	public GamePanel(int gameWidth, int gameHeight){
+		this.gameWidth = gameWidth;
+		this.gameHeight = gameHeight;
+		throweeList = new LinkedList<>();
+		canvas = new Canvas();
+		
+		canvas.setBackground(Color.white);
+		canvas.setSize(gameWidth,gameHeight);
+		
+		this.add(canvas);
 	}
-	
+	public void start(){
+		canvas.createBufferStrategy(2);//visible 이전에 하면 안됨
+		bufferStrategy = canvas.getBufferStrategy();
+	}
 }
