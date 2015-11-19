680bd9458d106c20a23f4459c525efc7f22eb34f
SOL KANG <anmenmolsu22@naver.com>
2015-11-19 20:49:40 +0900

    그래픽 그리기, 물리연산하기 틀 만듦

--- cutterGame/src/cutterGame/GamePanel.java
+++ cutterGame/src/cutterGame/GamePanel.java
@@ -2,6 +2,8 @@ package cutterGame;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferStrategy;
 import java.util.LinkedList;
 
@@ -17,7 +19,7 @@ public class GamePanel extends JPanel{
 	public GamePanel(int gameWidth, int gameHeight){
 		this.gameWidth = gameWidth;
 		this.gameHeight = gameHeight;
-		throweeList = new LinkedList<>();
+		
 		canvas = new Canvas();
 		
 		canvas.setBackground(Color.white);
@@ -26,7 +28,31 @@ public class GamePanel extends JPanel{
 		this.add(canvas);
 	}
 	public void start(){
+		throweeList = new LinkedList<>();
 		canvas.createBufferStrategy(2);//visible 이전에 하면 안됨
 		bufferStrategy = canvas.getBufferStrategy();
+		
+		physicsCalculation = new Thread(randomTaskRunnable());
+		physicsCalculation.start();
+		
+		Graphics2D g = (Graphics2D)bufferStrategy.getDrawGraphics();
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		while(true){
+			g.clearRect(0, 0, gameWidth, getHeight());
+			g.drawString("FPS: " + Math.random()*100, 0, 15);//change score
+			
+			for (Throwee throwee : throweeList) {
+				
+			}
+			
+			if(!bufferStrategy.contentsLost()) bufferStrategy.show();
+		}
+		
+	}
+	private Runnable randomTaskRunnable(){
+		
+		return ()->{
+			
+		};
 	}
 }
