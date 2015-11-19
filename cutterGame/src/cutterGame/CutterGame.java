cc08a1662e06deb8019ae53b5c22a3ad4b8871e4
SOL KANG <anmenmolsu22@naver.com>
2015-11-19 19:40:56 +0900

    add CardLayout, add start, game, end JPanel by SOL

--- cutterGame/src/cutterGame/CutterGame.java
+++ cutterGame/src/cutterGame/CutterGame.java
@@ -1,49 +1,63 @@
 package cutterGame;
 
+import java.awt.Canvas;
+import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferStrategy;
+import java.util.LinkedList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class CutterGame {
 	private final int gameWidth = 500, gameHeight = 350;
+
 	private JFrame mainFrame;
+	private CardLayout cardLayout ;
+	private Canvas canvas;
+	private BufferStrategy bufferStrategy;
+	private JPanel startPanel, gamePanel, endPanel;
+	private LinkedList<Throwee> throweeList;
+	private Thread physicsCalculation;
 	
 	public CutterGame(){
+		throweeList = new LinkedList<>();
+		cardLayout = new CardLayout();
 		mainFrame= new JFrame("CutterGame");
+		mainFrame.setLayout(cardLayout);
 		
+		canvas = new Canvas();
+		canvas.setBackground(Color.white);
+		canvas.setSize(gameWidth,gameHeight);
+
+		startPanel = new JPanel();//sung bin
+		gamePanel = new JPanel();
+		endPanel = new JPanel(); // wait...
+		
+//		startPanel.add
+		gamePanel.add(canvas);
+//		endPanel.add
+		
+
+		mainFrame.add(startPanel,"start");
+		mainFrame.add(gamePanel,"game");
+//		mainFrame.add(endPanel,"end");
 		
 		
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setSize(gameWidth, gameHeight);
 		mainFrame.setLocationRelativeTo(null);
+		mainFrame.pack();
 		mainFrame.setVisible(true);
+		
+		canvas.createBufferStrategy(2);//visible 이전에 하면 안됨
+		bufferStrategy = canvas.getBufferStrategy();
 	}
-	public void start() {//각종 jframe 기능 //재시작 및 종료 기능
+	public void start() {
+		cardLayout.show(mainFrame.getContentPane(),"game"); //아직 start 아님
 		
 		
-	}
-	
-	class StartButtonJPanel extends JPanel implements MouseListener{
-
-		@Override
-		public void mouseClicked(MouseEvent e) {
-		}
-
-		@Override
-		public void mouseEntered(MouseEvent e) {
-		}
-
-		@Override
-		public void mouseExited(MouseEvent e) {
-		}
-
-		@Override
-		public void mousePressed(MouseEvent e) {
-		}
-		@Override
-		public void mouseReleased(MouseEvent e) {
-		}
 	}
 }
